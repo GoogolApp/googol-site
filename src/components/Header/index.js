af7708ca29358b9d8476e8718dbc495139340fa3
matheusps <matheus.ps@icloud.com>
2019-03-16 17:57:58 -0300

    -m

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header className="flex bg-black white pa2">
+    <h2>Googol</h2>
+  </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
+import Header from '../components/Header'
 
-export default () => <div>Hello world!</div>
+import 'tachyons/css/tachyons.min.css'
+import './style.css'
+
+export default () => (
+  <div className="home flex flex-column white min-vh-100 bg-near-black">
+    <Header />
+    Hello world!
+  </div>
+)
